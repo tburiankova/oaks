fdcefa5c910ec082ff86871e212d69ea5218558f
Tereza Buriankova <buriankova@pixel-mate.com>
2021-07-06 16:14:45 +0200

    progres phases data, logic, mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,112 @@
 import React, { useState } from 'react';
-import style from './App.module.scss';
 import Checkbox from './components/Checkbox';
+import style from './App.module.scss';
+import { classes } from './services/utils';
 
 function App() {
-   const [testCheck, setTestCheck] = useState(false);
+   const [state, setState] = useState(
+      progressData.reduce((acc, val) => {
+         acc[val.name] = val.steps.reduce((acc2, val2) => {
+            acc2[val2.id] = { ...val2, value: false };
+            return acc2;
+         }, {});
+         return acc;
+      }, {}),
+   );
+
+   const handleChange = (phase: string, stepId: number) => {
+      setState({
+         ...state,
+         [phase]: {
+            ...state[phase],
+            [stepId]: {
+               ...state[phase][stepId],
+               value: !state[phase][stepId].value,
+            },
+         },
+      });
+   };
 
    return (
       <div className={style['page-container']}>
-         <Checkbox
-            name="test"
-            checked={testCheck}
-            onChange={() => setTestCheck(!testCheck)}
-         >
-            test
-         </Checkbox>
-         <Checkbox
-            name="test"
-            checked={true}
-            onChange={() => setTestCheck(!testCheck)}
-            disabled
-         >
-            test
-         </Checkbox>
+         <div className={style.container}>
+            <h1>My startup progress</h1>
+
+            {/* <pre>{JSON.stringify(state, null, 2)}</pre> */}
+
+            <div className={style.inner}>
+               {progressData.map(({ id, name, steps }, i) => {
+                  const prevPhase = i === 0 ? null : progressData[i - 1].name;
+                  const unlocked = prevPhase
+                     ? Object.keys(state[prevPhase]).every(
+                          (step) => state[prevPhase][step].value,
+                       )
+                     : true;
+                  const isComplete = Object.keys(state[name]).every(
+                     (step) => state[name][step].value,
+                  );
+
+                  return (
+                     <div
+                        key={id}
+                        className={classes(style.progress, {
+                           [style['is-complete']]: isComplete,
+                        })}
+                     >
+                        <h2>
+                           <span>{id}</span>
+                           {name}
+                        </h2>
+                        <ul>
+                           {steps.map(({ id, title }) => (
+                              <li key={id}>
+                                 <Checkbox
+                                    name={title}
+                                    checked={state[name][id].value}
+                                    onChange={() => handleChange(name, id)}
+                                    disabled={!unlocked}
+                                 >
+                                    {title}
+                                 </Checkbox>
+                              </li>
+                           ))}
+                        </ul>
+                     </div>
+                  );
+               })}
+            </div>
+         </div>
       </div>
    );
 }
 
 export default App;
+
+const progressData = [
+   {
+      id: 1,
+      name: 'Foundation',
+      steps: [
+         { id: 1, title: 'Setup virtual office' },
+         { id: 2, title: 'Set mission & vision' },
+         { id: 3, title: 'Select business name' },
+         { id: 4, title: 'Buy domains' },
+      ],
+   },
+   {
+      id: 2,
+      name: 'Discovery',
+      steps: [
+         { id: 1, title: 'Create roadmap' },
+         { id: 2, title: 'Competitor analysis' },
+      ],
+   },
+   {
+      id: 3,
+      name: 'Delivery',
+      steps: [
+         { id: 1, title: 'Release marketing website' },
+         { id: 2, title: 'Release MVP' },
+      ],
+   },
+];
